c6f2d4cc641608ac9f246f482734860befa24bd6
atsukoba <atsuya_wgp4346@keio.jp>
2019-10-25 17:25:19 +0900

    ♬"talking": play received sound on another thread♬

--- service/app.py
+++ service/app.py
@@ -1,12 +1,17 @@
 import json
 import os
-
+import threading
+import wave
 from datetime import datetime
+
 from flask import Flask, abort, jsonify, render_template, request
-from flask_ngrok import run_with_ngrok
+
+import pyaudio
 
 app = Flask(__name__)
-# run_with_ngrok(app)
+
+with open("config.json", "r") as f:
+    conf = json.load(f)
 
 
 @app.route('/', methods=['GET'])
@@ -17,12 +22,38 @@ def root():
 
 @app.route('/', methods=['POST'])
 def upload():
-    fname = datetime.now().strftime('%m%d%H%M%S') + ".wav"
-    with open(f"sounds/{fname}", "wb") as f:
+    fname = "sounds/" + datetime.now().strftime('%m%d%H%M%S') + ".wav"
+    with open(f"{fname}", "wb") as f:
         f.write(request.files['data'].read())
     print("posted binary data")
+    if conf["talking"]:
+        player = threading.Thread(target=play_wav_file, args=(fname, ))
+        player.start()
     return jsonify({"data": fname})
 
+
+def play_wav_file(fname):  # talking mode
+    try:
+        wf = wave.open(fname, "r")
+    except FileNotFoundError:
+        print("[Error 404] No such file or directory: " + fname)
+        return 0
+
+    p = pyaudio.PyAudio()
+    print("talking: open audio stream")
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    print("talking: loading data")
+    data = wf.readframes(1024)
+    print(wf)
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(1024)
+    stream.close()
+    p.terminate()
 
 if __name__ == "__main__":
     app.run()
