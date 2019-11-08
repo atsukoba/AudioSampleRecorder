23960f37816a26e8572159892508d968e376d2f9
atsukoba <atsuya_wgp4346@keio.jp>
2019-11-08 18:36:48 +0900

    fixed QR code error on https protocol

--- qr.py
+++ qr.py
@@ -2,9 +2,6 @@ import qrcode
 
 
 def generate(url):
-    if url[4] == "s":
-        print("QR will be made with `https` url")
-        url = "https" + url[4:]
     img = qrcode.make(url)
     print("Generating QR Code...")
     img.show()
